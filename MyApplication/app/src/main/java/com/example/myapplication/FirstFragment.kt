f1da8dba5c203f9e6efcf1df4d5603b61f129524
Thiago Maia da Costa <thiago.maia971@hotmail.com>
2020-03-13 22:07:05 -0300

    tela de login

---
+++ MyApplication/app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        view.findViewById<Button>(R.id.registrarBtn).setOnClickListener{
+            Log.d("btnSetup", "Selected")
+            findNavController().navigate(R.id.action_FirstFragment_to_registerActivity)
+        }
+    }
+}
